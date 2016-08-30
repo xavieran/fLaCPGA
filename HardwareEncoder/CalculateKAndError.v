9fe74d50f13dbd319ec733bcf663e2f1abbdd640
Emmanuel Jacyna <xavieran.lives@gmail.com>
2016-08-30 20:41:11 +1000

    Implemented error and km calculator

--- HardwareEncoder/CalculateKAndError.v
+++ HardwareEncoder/CalculateKAndError.v
@@ -5,302 +5,111 @@ module CalculateKAndError (
     input wire iEnable, 
     input wire iReset,
     
-    input wire iValid,
+    input wire [31:0] iAlpham,
+    input wire [31:0] iKm, // K_m
+    input wire [31:0] iErrorm, // E_m
     
-    input wire [31:0] iACF1,
-    input wire [31:0] iACF2,
-    input wire [31:0] iModel1,
-    input wire [31:0] iModel2,
-    
-    output wire [31:0] oAlpha,
+    output wire [31:0] oKmp1, // K_m+1
+    output wire [31:0] oErrormp1,// E_m+1
     output wire oDone
     );
 
+// Divide Multiply Multiply Add == 14 + 5 + 5 + 7 = 31 cycles latency
+
 integer i;
-parameter ORDER = 12;
+parameter DIV_LATENCY = 14;
 parameter MULT_LATENCY = 5;
 parameter ADD_LATENCY = 7;
-parameter TOTAL_LATENCY = MULT_LATENCY + ADD_LATENCY;
+parameter TOTAL_LATENCY = DIV_LATENCY + MULT_LATENCY + MULT_LATENCY + ADD_LATENCY + 5;
+wire [31:0] neg_alpha = iAlpham ^ 32'h80000000; // flip sign bit to obtain negative alpha
 
-//Valid;
+reg calculate;
 reg done;
+reg [7:0] counter;
 
-reg rValid;
-reg [31:0] dValid;
-reg [3:0] input_count;
+reg [31:0] alpha, km, errorm;
 
+reg [31:0] o_error;
+reg [31:0] o_k;
 
-reg [31:0] alpha;
+reg [31:0] kmp1, kmp1_sq, errormp1, mult_term;
 
-reg [31:0] rACF1, rACF2, rModel1, rModel2;
+wire [31:0] d1_out, m1_out, m2_out, s1_out;
 
-reg [31:0] partial_sums_stage1a [0:3];
-reg [31:0] partial_sums_stage1b [0:3];
-
-reg [31:0] partial_sums_stage2a [0:1];
-reg [31:0] partial_sums_stage2b [0:1];
-
-reg [31:0] partial_sums_stage3a;
-reg [31:0] partial_sums_stage3b;
-
-reg [3:0] stage;
-
-reg [2:0] stage2_in_sel;
-reg [1:0] stage3_in_sel;
-
-reg [31:0] stage2_add_count;
-reg [31:0] stage3_add_count;
-reg [31:0] stage4_add_count;
-
-reg flip;
-
-
-reg [31:0] a2_i1, a2_i2, a2_i1_s2, a2_i1_s3, a2_i2_s2, a2_i2_s3;
-
-assign oAlpha = alpha;
+assign oKmp1 = o_k;
+assign oErrormp1 = o_error;
 assign oDone = done;
 
-wire [31:0] m1_out, m2_out, a1_out, a2_out;
-
-wire s2Valid = stage2_in_sel < ((input_count + 1'b1) >> 1);
-wire s3Valid = stage3_in_sel < 1; // ???
-
-wire mult_en = iEnable;
-wire a1_en = iEnable;
-wire a2_en = iEnable;
+fp_divider div (
+    .clk_en(calculate),
+    .clock(iClock),
+    .dataa(alpha),
+    .datab(errorm),
+    .result(d1_out));
 
 fp_mult m1 (
-    .clk_en(mult_en),
+    .clk_en(calculate),
     .clock(iClock),
-    .dataa(rACF1),
-    .datab(rModel1),
+    .dataa(kmp1),
+    .datab(kmp1),
     .nan(),
     .result(m1_out));
 
 fp_mult m2 (
-    .clk_en(mult_en),
+    .clk_en(calculate),
     .clock(iClock),
-    .dataa(rACF2),
-    .datab(rModel2),
+    .dataa(errorm),
+    .datab(kmp1_sq),
     .nan(),
     .result(m2_out));
 
-fp_add_sub a1 (
-    .add_sub(1'b1),
-    .clk_en(a1_en),
+// Subtractor...
+fp_add_sub s1 (
+    .add_sub(1'b0),
+    .clk_en(calculate),
     .clock(iClock),
-    .dataa(m1_out),
-    .datab(m2_out),
-    .result(a1_out));
-
-fp_add_sub a2 (
-    .add_sub(1'b1),
-    .clk_en(a2_en),
-    .clock(iClock),
-    .dataa(a2_i1),
-    .datab(a2_i2),
-    .result(a2_out));
-
-// 3f800000 1.0
+    .dataa(errorm),
+    .datab(mult_term),
+    .result(s1_out));
 
 always @(posedge iClock) begin
     if (iReset) begin
         done <= 0;
+        counter <= 0;
+        calculate <= 1'b0;
+        
+        o_error <= 0;
+        o_k <= 0;
+        
         alpha <= 0;
-        rACF1 <= 0;
-        rACF2 <= 0;
-        rModel1 <= 0;
-        rModel2 <= 0;
-        rValid <= 0;
-        dValid <= 0;
-        input_count <= 0;
-        
-        for (i = 3; i >= 0; i = i - 1) begin
-            partial_sums_stage1a[i] <= 0;
-            partial_sums_stage1b[i] <= 0;
-        end
-        
-        for (i = 1; i >= 0; i = i - 1) begin
-            partial_sums_stage2a[i] <= 0;
-            partial_sums_stage2b[i] <= 0;
-        end
-        
-        
-        partial_sums_stage3a <= 0;
-        partial_sums_stage3b <= 0;
-        
-        flip <= 0;
-        
-        
-        stage <= 0;
-        stage2_in_sel <= 0;
-        stage3_in_sel <= 0;
-        stage2_add_count <= 1;
-        stage3_add_count <= 1;
-        stage4_add_count <= 1;
-        
-       
+        km <= 0;
+        errorm <= 0;
+        kmp1 <= 0;
+        kmp1_sq <= 0;
+        mult_term <= 0;
+        errormp1 <= 0;
     end else if (iEnable) begin
-        rACF1 <= iACF1;
-        rACF2 <= iACF2;
-        rModel1 <= iModel1;
-        rModel2 <= iModel2;
-        rValid <= iValid;
-        
-        if (rValid) begin
-            input_count <= input_count + 1'b1;
+        if (counter < TOTAL_LATENCY) begin
+            counter <= counter + 1'b1;
+            calculate <= 1'b1;
         end else begin
-            input_count <= input_count;
+            done <= 1'b1;
+            calculate <= 1'b0;
+            o_error <= errormp1;
+            o_k <= kmp1;
         end
         
-        dValid = (dValid << 1) | rValid;
+        alpha <= neg_alpha;
+        km <= iKm;
+        errorm <= iErrorm;
         
-        // If the data out of the first adder is valid, then shift it down
-        if (dValid[TOTAL_LATENCY]) begin
+        kmp1 <= d1_out;
         
-            if (flip) begin
-                flip <= 0;
-                for (i = 3; i > 0; i = i - 1) begin
-                    partial_sums_stage1a[i] <= partial_sums_stage1a[i - 1];
-                end
-                partial_sums_stage1a[0] <= a1_out;
-            end else begin
-                flip <= 1;
-                for (i = 3; i > 0; i = i - 1) begin
-                    partial_sums_stage1b[i] <= partial_sums_stage1b[i - 1];
-                end
-                partial_sums_stage1b[0] <= a1_out;
-            end
-            
-            // On falling edge o
-            if (dValid[TOTAL_LATENCY:TOTAL_LATENCY-1] == 2'b10) begin
-                stage <= 1;
-            end 
-            
-        end else if (stage == 1) begin
-            /* Start counting in the next to be added */
-            if (s2Valid) begin
-                stage2_in_sel <= stage2_in_sel + 1'b1;
-            end
-            
-            stage2_add_count <= (stage2_add_count << 1) | s2Valid;
-            
-            /* Start saving them into the next stage*/
-            if (stage2_add_count[ADD_LATENCY - 1]) begin
-                if (flip) begin
-                    flip <= 0;
-                    partial_sums_stage2a[1] <= partial_sums_stage2a[0];
-                    partial_sums_stage2a[0] <= a2_out;
-                end else begin
-                    flip <= 1;
-                    partial_sums_stage2b[1] <= partial_sums_stage2b[0];
-                    partial_sums_stage2b[0] <= a2_out;
-                end
-            end
-            
-            // On falling edge of valid signal, we move to next stage
-            if (stage2_add_count[ADD_LATENCY-1:ADD_LATENCY - 2] == 2'b10) begin
-                stage <= 2;
-            end
-            
-        end else if (stage == 2) begin
-            /* Start counting in the next to be added */
-            if (s3Valid) begin
-                stage3_in_sel <= stage3_in_sel + 1'b1;
-            end
-            
-            stage3_add_count <= (stage3_add_count << 1) | s3Valid;
-            
-            /* Start saving them into the next stage*/
-            if (stage3_add_count[ADD_LATENCY]) begin
-                if (flip) begin
-                    flip <= 0;
-                    partial_sums_stage3a <= a2_out;
-                end else begin
-                    flip <= 1;
-                    partial_sums_stage3b <= a2_out;
-                end
-            end
-            
-            // On falling edge of valid signal, we move to next stage
-            if (stage3_add_count[ADD_LATENCY:ADD_LATENCY - 1] == 2'b10) begin
-                stage <= 3;
-            end
-            
-        end else if (stage == 3) begin
-            stage4_add_count <= (stage4_add_count << 1);
-            /* Start saving them into the next stage*/
-            if (stage4_add_count[ADD_LATENCY]) begin
-                alpha <= a2_out;
-                done <= 1'b1;
-                stage <= 4;
-            end 
-        end
-    end 
-end
-
-
-
-
-always @(stage or a2_i1_s2 or a2_i1_s3) begin
-    if (stage == 1) begin
-        a2_i1 <= a2_i1_s2;
-    end else if (stage == 2) begin
-        a2_i1 <= a2_i1_s3;
-    end else if (stage == 3) begin
-        a2_i1 <= partial_sums_stage3a;
-    end else begin
-        a2_i1 <= 0;
-    end
-end
-
-always @(stage or a2_i2_s2 or a2_i2_s3) begin
-    if (stage == 1) begin
-        a2_i2 <= a2_i2_s2;
-    end else if (stage == 2) begin
-        a2_i2 <= a2_i2_s3;
-    end else if (stage == 3) begin
-        a2_i2 <= partial_sums_stage3b;
-    end else begin
-        a2_i2 <= 0;
-    end
-end
-
-/* Muxes for second stage adder input */
-always @(stage2_in_sel or partial_sums_stage1a) begin
-    if (stage2_in_sel < 4) begin
-        a2_i1_s2 <= partial_sums_stage1a[stage2_in_sel];
-    end else begin
-        a2_i1_s2 <= 0;
-    end
-end
-
-always @(stage2_in_sel or partial_sums_stage1b) begin
-    if (stage2_in_sel < 4) begin
-        a2_i2_s2 <= partial_sums_stage1b[stage2_in_sel];
-    end else begin
-        a2_i2_s2 <= 0;
-    end
-end
-
-/* Muxes for third stage adder input */
-always @(stage3_in_sel or partial_sums_stage2a) begin
-    if (stage3_in_sel == 0) begin
-        a2_i1_s3 <= partial_sums_stage2a[0];
-    end else if (stage3_in_sel == 1) begin
-        a2_i1_s3 <= partial_sums_stage2a[1];
-    end else begin
-        a2_i1_s3 <= 0;
-    end 
-end
-
-always @(stage3_in_sel or partial_sums_stage2b) begin
-    if (stage3_in_sel == 0) begin
-        a2_i2_s3 <= partial_sums_stage2b[0];
-    end else if (stage3_in_sel == 1) begin
-        a2_i2_s3 <= partial_sums_stage2b[1];
-    end else begin
-        a2_i2_s3 <= 0;
+        kmp1_sq <= m1_out;
+        
+        mult_term <= m2_out;
+        
+        errormp1 <= s1_out;
     end
 end
 
---
+++ HardwareEncoder/CalculateKAndErrorTB.v
@@ -0,0 +1,56 @@
+`timescale 1ns/100ps
+`define MY_SIMULATION 1
+
+module CalculateKAndErrorTB;
+
+reg clk, ena, rst;
+integer i;
+integer cycles;
+reg [31:0] km, alpham, errorm;
+
+wire [31:0] kmp1, errormp1;
+wire done;
+
+CalculateKAndError ckae(
+    .iClock(clk),
+    .iEnable(ena), 
+    .iReset(rst),
+    
+    .iAlpham(alpham),
+    .iKm(km), // K_m
+    .iErrorm(errorm), // E_m
+    
+    .oKmp1(kmp1), // K_m+1
+    .oErrormp1(errormp1),// E_m+1
+    .oDone(done)
+    );
+
+always begin
+    #0 clk = 0;
+    #10 clk = 1;
+    #10 cycles = cycles + 1;
+end
+
+initial begin
+    cycles = 0;
+    ena = 0; rst = 1;
+    #30;    
+    alpham = 32'h3ca3d70a; // .02
+    km = 32'h3f99999a; // 1.2
+    errorm = 32'h3f666666; // .9
+    #20; 
+    
+    ena = 1; rst = 0;
+    
+    for (i = 0; i < 200; i = i + 1) begin
+        if (done) begin
+            $display("cycles: %d kmp1: %f emp1: %f", cycles,$bitstoshortreal(kmp1),$bitstoshortreal(errormp1));
+            $stop;
+        end
+        #20 ;
+    end
+    $stop;
+    
+end
+
+endmodule
