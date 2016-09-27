11cc1158ff7be7400d492356098fb894a995214f
Emmanuel Jacyna <xavieran.lives@gmail.com>
2016-09-28 01:03:45 +1000

    Added paper

--- Software/src/matlab/levinson_durbin.m
+++ Software/src/matlab/levinson_durbin.m
@@ -2,18 +2,19 @@
 % The following flac command was used
 % flac -V -b 4096 -l 12  -A hamming ../../audio/Pavane16Blocks.wav -f
 % resulting coefficients were 
-format long
-flac_qlp = [1427,-154,416,-517,-134,-281,-54,136,140,131,116,-206]
+%format long
+%flac_qlp = [1427,-154,416,-517,-134,-281,-54,136,140,131,116,-206]
 
 
 data = dlmread('Pavane16Blocks.txt');
 % Window using hamming window...
-data = data(1:4096);%.*hamming(4096);
+data = data(1:end);%.*hamming(4096);
 %data = data(1+4096:4096+4096);%.*hamming(4096);
 
-order = 12;
+order = 50;
+blocks = 16;
 
-ACF =  my_autocorr(data, order)
+
 
 %%
 % Ra = b
@@ -32,16 +33,30 @@ ACF =  my_autocorr(data, order)
 %A = inv(R)*b;
 %A = [1, A']
 
-A = my_levinson(ACF, order);
-A = A(13,:)
+residuals = zeros(size(data));
+for i = 1:blocks
+    ACF = autocorr(data(4096*(i-1) + 1:4096*i).*hamming(4096), order)
+    A = my_levinson(ACF, order);
+    A = A(order,:);
+    A = round(A*-1024);
+
+    coeffs = int32((A(2:end)))
+    residuals(4096*(i-1) + 1:4096*i) = my_fir_filter(coeffs, data(4096*(i-1) + 1:4096*i));
+    
+end
+
+hold on
+plot(data, 'r.')
+plot(residuals, 'g*')
+
+play = double(residuals);
+play = play./max(play);
+play = .6*play;
+sound(play, 44100)
 
 
-filtered = filter([0, -A(2:order)], 1, data);
 
-%sound(filtered - data, 44100)
+%plot(1:order, A(2:end)*flac_qlp(1)/A(2), 'ob',1:order,flac_qlp, '+r')
+%legend('Me', 'FLAC');
+%title('Calculated model and FLAC quantized model');
 
-plot(1:order, A(2:end)*flac_qlp(1)/A(2), 'ob',1:order,flac_qlp, '+r')
-legend('Me', 'FLAC');
-title('Calculated model and FLAC quantized model');
-
-round(A*-1024)
